f182b98103a2a862858b794a7cca121919280a3d
Khaja0802 <skhajamoinuddin0802@gmail.com>
2024-09-10 11:04:35 +0000

    react-router-DOM installed

--- IMDB-Clone/src/App.jsx
+++ IMDB-Clone/src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Movies from "./Components/Movies";
+import WatchList from "./Components/WatchList";
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Navbar/>
+    <Routes>
+      <Route path="/" element={<Movies/>}/>
+      <Route path="/watchlist" element={<WatchList/>}/>
+
+
+    </Routes>
+    </BrowserRouter>
     
 
 
--- IMDB-Clone/src/Components/Navbar.jsx
+++ IMDB-Clone/src/Components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Logo from '../MovieLogo.png'
+import {Link} from "react-router-dom"
 
 
 const Navbar = () => {
     return (
         <div className='flex border space-x-8 items-center pl-3 py-4'>
             <img className='w-[50px]' src={Logo} alt=""/>
-            <a href='/' className='text-amber-500 text-3xl font-bold'>Home</a>
-            <a href='/watchlist' className='text-amber-500 text-3xl font-bold'>Watchlist</a>
+            <Link to='/' className='text-amber-500 text-3xl font-bold'>Home</Link>
+            <Link to='/watchlist' className='text-amber-500 text-3xl font-bold'>Watchlist</Link>
         </div>
     )
 }
